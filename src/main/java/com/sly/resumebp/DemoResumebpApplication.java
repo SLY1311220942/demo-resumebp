e32109612e89d9ed584c66f913bc6c0864d9a181
sly <1311220942@qq.com>
2019-03-04 21:31:47 +0800

    初始提交

---
+++ src/main/java/com/sly/resumebp/DemoResumebpApplication.java
@@ -0,0 +1,13 @@
+package com.sly.resumebp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoResumebpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoResumebpApplication.class, args);
+	}
+
+}
