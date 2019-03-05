273a4485a69e5cd8546927c8c04df5c76f54eb55
盛领宜 <sheng.ly_wh@www.sand.com>
2019-03-05 18:00:34 +0800

    提交

--- src/main/java/com/sly/resumebp/controller/SingleThreadUploadController.java
+++ src/main/java/com/sly/resumebp/controller/SingleThreadUploadController.java
@@ -1,12 +1,22 @@
 package com.sly.resumebp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.sly.resumebp.common.result.BaseResult;
 
 /**
  * _单线程上传controller
+ * 
  * @author sly
  * @time 2019年3月5日
  */
@@ -14,5 +24,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/singleThread")
 public class SingleThreadUploadController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SingleThreadUploadController.class);
-}
 
+	@ResponseBody
+	@RequestMapping("/upload")
+	public BaseResult upload(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			// 获取上传文件
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			MultipartFile multipartFile = multipartRequest.getFile("file");
+			
+			return null;
+		} catch (Exception e) {
+			LOGGER.error(ExceptionUtils.getStackTrace(e));
+			return new BaseResult(400, "上传失败!");
+		}
+	}
+}
--- src/test/java/com/sly/resumebp/DemoResumebpApplicationTests.java
+++ src/test/java/com/sly/resumebp/DemoResumebpApplicationTests.java
@@ -1,16 +1,181 @@
 package com.sly.resumebp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.junit.Test;
+import org.springframework.expression.AccessException;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 public class DemoResumebpApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	/**
+	 * _普通读写
+	 * 
+	 * @author sly
+	 * @time 2019年3月5日
+	 */
+	@Test
+	public void test01() {
+		// 源文件与目标文件
+		File sourcefile = new File("D:/test/source_resumebp.txt");
+		File targetfile = new File("D:/test/target_resumebp.txt");
+
+		// 输入输出流
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+
+		// 数据缓冲区
+		byte[] buf = new byte[5];
+		int len = 0;
+		try {
+			inputStream = new FileInputStream(sourcefile);
+			outputStream = new FileOutputStream(targetfile);
+			while ((len = inputStream.read(buf)) > 0) {
+				System.out.println(len + " write data ...");
+				outputStream.write(buf, 0, len);
+			}
+
+		} catch (Exception e) {
+			System.out.println(ExceptionUtils.getStackTrace(e));
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					System.out.println(ExceptionUtils.getStackTrace(e));
+				}
+			}
+
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					System.out.println(ExceptionUtils.getStackTrace(e));
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * _模拟传输中断后继续传输
+	 * 
+	 * @author sly
+	 * @time 2019年3月5日
+	 */
+	@Test
+	public void test02() {
+		// 源文件与目标文件
+		File sourcefile = new File("D:/test/source_resumebp.txt");
+		File targetfile = new File("D:/test/target_resumebp.txt");
+
+		// 输入输出流
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+
+		// 数据缓冲区
+		byte[] buf = new byte[5];
+		int len = 0;
+		// 记录传输位置
+		int position = 0;
+		try {
+			inputStream = new FileInputStream(sourcefile);
+			outputStream = new FileOutputStream(targetfile);
+
+			while ((len = inputStream.read(buf)) > 0) {
+				// 传输了3段内容后忘了中断
+				if (position == 3) {
+					throw new AccessException("传输中断!");
+				}
+				System.out.println(len + " write data ...");
+				outputStream.write(buf, 0, len);
+				position++;
+			}
+
+		} catch (AccessException e) {
+			keepGoing(sourcefile, targetfile, position);
+		} catch (Exception e) {
+			System.out.println(ExceptionUtils.getStackTrace(e));
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException e) {
+					System.out.println(ExceptionUtils.getStackTrace(e));
+				}
+			}
+
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					System.out.println(ExceptionUtils.getStackTrace(e));
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * _继续传输
+	 * 
+	 * @param sourcefile
+	 * @param targetfile
+	 * @param position
+	 * @author sly
+	 * @time 2019年3月5日
+	 */
+	public void keepGoing(File sourcefile, File targetfile, int position) {
+		try {
+			// 线程休眠30秒
+			Thread.sleep(30000);
+		} catch (Exception e) {
+			System.out.println(ExceptionUtils.getStackTrace(e));
+		}
+		
+		RandomAccessFile readFile = null;
+		RandomAccessFile writeFile = null;
+		try {
+			readFile = new RandomAccessFile(sourcefile, "r");
+			writeFile = new RandomAccessFile(targetfile, "rw");
+
+			readFile.seek(5 * position);
+			writeFile.seek(5 * position);
+			
+			byte[] buf = new byte[5];
+			int len = 0;
+			while ((len = readFile.read(buf)) > 0) {
+				writeFile.write(buf, 0, len);
+			}
+		} catch (Exception e) {
+			System.out.println(ExceptionUtils.getStackTrace(e));
+		} finally {
+			if(readFile != null) {
+				try {
+					readFile.close();
+				} catch (IOException e) {
+					System.out.println(ExceptionUtils.getStackTrace(e));
+				}
+			}
+			if(writeFile != null) {
+				try {
+					writeFile.close();
+				} catch (IOException e) {
+					System.out.println(ExceptionUtils.getStackTrace(e));
+				}
+			}
+		}
+	}
 }
